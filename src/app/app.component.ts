f40a8617c7c3d432d19d61e1bb0de53031e1c002
hetviparekh01 <110492506+hetviparekh01@users.noreply.github.com>
2024-06-26 09:45:43 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'content_management_system-frontend';
+}
